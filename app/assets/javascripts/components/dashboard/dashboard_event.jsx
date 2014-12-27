b4932ab8034eedc115051042dbaf51af73ed1ed2
Sunny Vempati <sunnyvempati@gmail.com>
2014-12-27 00:02:52 -0600

    events stuff

--- app/assets/javascripts/components/dashboard/dashboard_event.jsx
+++ app/assets/javascripts/components/dashboard/dashboard_event.jsx
@@ -1,8 +1,23 @@
 var DashboardEvent = React.createClass({
 	render: function() {
 		return (
-			<div>
-				{this.props.event.name}
+			<div className="dashboard-event">
+				<div className="dashboard-event-header">
+					<div className="dashboard-event-header-name">
+						{this.props.event.name}
+					</div>
+					<div className="dashboard-event-header-date">
+						{this.props.event.start_date}	
+					</div>
+				</div>
+				<div className="dashboard-event-content">
+					<div className="dashboard-event-client-panel">
+					</div>
+					<div className="dashboard-event-tasks-panel">
+					</div>
+					<div className="dashboard-event-messages-panel">
+					</div>
+				</div>
 			</div>
 		);
 	}
