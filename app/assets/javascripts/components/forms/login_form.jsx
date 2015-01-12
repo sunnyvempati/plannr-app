890a58d9d2a80bcfb65dd3676541a934f41595e1
Sunny Vempati <sunnyvempati@gmail.com>
2015-01-11 21:51:06 -0600

    completed fixing login screen

--- app/assets/javascripts/components/forms/login_form.jsx
+++ app/assets/javascripts/components/forms/login_form.jsx
@@ -1,16 +1,15 @@
 var LoginForm = React.createClass({
 	render: function() {
 		return (
-			<div className="form-container">
-				<Form action={this.props.action} method="post" id="new_user">
+			<div className="login-form-container">
+				<Form action={this.props.action} method="post" id="new_user" submitBtnText="login" submitBtnClass="login-form-btn">
 					<HiddenAuthFields auth_param={this.props.auth_param} auth_token={this.props.auth_token} />
-					<FormInput name="user[email]" autofocus="autofocus" placeholder="sunny@yourplannr.com" type="email" />
-					<FormInput name="user[password]" autofocus="off" placeholder="password" type="password" />
-					<Button type="submit">
-						Login
-					</Button>
+					<FormInput name="user[email]" autofocus="autofocus" placeholder="sunny@yourplannr.com" type="email" label="email" />
+					<FormInput name="user[password]" autofocus="off" placeholder="password" type="password" label="password" />
 				</Form>
 			</div>
+			
 		);
 	}
+
 });
--- app/assets/javascripts/components/generic/form.jsx
+++ app/assets/javascripts/components/generic/form.jsx
@@ -2,14 +2,12 @@ var Form = React.createClass({
 	render: function() {
 		var all_props = this.props;
 		return (
-			<div className="form">
-				<div className="form-header"></div>
-				<div className="form-content">
-					<form acceptCharset="UTF-8" {...all_props}>
-						{this.props.children}
-					</form>
-				</div>
-			</div>
+			<form acceptCharset="UTF-8" {...all_props}>
+				{this.props.children}
+				<Button type="submit" className={this.props.submitBtnClass}>
+					{this.props.submitBtnText}
+				</Button>
+			</form>
 		);
 	}
 });
