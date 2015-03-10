1517331e7365a8ca634979c3a640dcb828398f3d
Sunny Vempati <sunnyvempati@gmail.com>
2015-03-10 00:20:19 -0600

    fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,10 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
+
+# database config
+set :rails_env, 'staging'
+
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
