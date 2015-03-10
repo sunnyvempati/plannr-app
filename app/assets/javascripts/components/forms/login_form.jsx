95cb333f6fd67ead9386150c59eda9e1b3280de9
Sunny Vempati <sunnyvempati@gmail.com>
2015-03-10 17:34:37 -0600

    login page

--- app/assets/javascripts/components/forms/login_form.jsx
+++ app/assets/javascripts/components/forms/login_form.jsx
@@ -4,7 +4,7 @@ var LoginForm = React.createClass({
       <div className="LoginFormContainer">
         <Form action={this.props.action} method="post" id="new_user" submitBtnText="login" submitBtnClass="Button LoginForm-button">
           <HiddenAuthFields auth_param={this.props.auth_param} auth_token={this.props.auth_token} />
-          <FormInput name="user[email]" autofocus="autofocus" placeholder="sunny@yourplannr.com" type="email" label="email" />
+          <FormInput name="user[email]" autofocus="autofocus" placeholder="email" type="email" label="email" />
           <FormInput name="user[password]" autofocus="off" placeholder="password" type="password" label="password" />
         </Form>
       </div>
