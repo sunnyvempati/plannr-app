87feac11e2bfbe0e4a66a2f6cf80db60e83cd39f
JustinK322 <justin@yourplannr.com>
2015-03-12 14:35:23 -0600

    Refactored create and edit task forms into task form
    task_form.jsx now replaces create_task_form.jsx and edit_task_form.jsx

---
+++ app/assets/javascripts/components/forms/task/task_form.jsx
@@ -0,0 +1,47 @@
+var TaskForm = React.createClass({
+    displayName: "TaskForm",
+
+    getInitialState: function () {
+        //return populated object on edit/update methods put or patch
+        var retStateObject = {
+            name: null,
+            description: null,
+            deadline: null
+        }
+
+        if (this.props.method.toLowerCase() === 'patch' || this.props.method.toLowerCase() === 'put') {
+            retStateObject.name = this.props.task.name;
+            retStateObject.description = this.props.task.description;
+            retStateObject.deadline = this.props.task.deadline;
+        }
+
+        return retStateObject;
+    },
+
+    onChangeName: function (e) {
+        this.setState({name: e.target.value});
+    },
+
+    onChangeDescription: function (e) {
+        this.setState({description: e.target.value});
+    },
+
+    onChangeDeadline: function (e) {
+        this.setState({deadline: e.target.value});
+    },
+
+    render: function () {
+        return (
+            <Form action={this.props.action} method={this.props.formMethod} id={this.props.formId} submitBtnText={this.props.submitButtonText}>
+                <input name="_method" type="hidden" value={this.props.method} />
+                <HiddenAuthFields auth_param={this.props.auth_param} auth_token={this.props.auth_token} />
+                <FormInput name="task[name]" autofocus="autofocus" placeholder="What is the name of your task?" type="text" label="name" value={this.state.name} onChange={this.onChangeName} />
+                <FormInput name="task[description]" autofocus="off" placeholder="How would you describe this task?" type="text" label="description" value={this.state.description} onChange={this.onChangeDescription} />
+                <FormInput name="task[deadline]" autofocus="off" placeholder="What is the deadline for this task? (DD/MM/YYYY)" type="datetime" label="deadline" value={this.state.deadline} onChange={this.onChangeDeadline} />
+
+            </Form>
+        );
+    }
+
+
+});
--- app/assets/javascripts/components/generic/hidden_auth_field.jsx
+++ app/assets/javascripts/components/generic/hidden_auth_field.jsx
@@ -2,7 +2,7 @@ var HiddenAuthFields = React.createClass({
   render: function() {
     var style = {display: 'none'};
     return (
-      <div style={style}>
+  <div style={style}>
         <input name="utf8" type="hidden" value="✓" />
         <input type="hidden" name={this.props.auth_param} value={this.props.auth_token} />
       </div>
