6220bc3a0adf465ec0eac4b311665955fb39d6f0
JustinK322 <justin@yourplannr.com>
2015-03-13 12:14:49 -0600

    change show/hide for button list to boolean from string

--- app/assets/javascripts/components/generic/button_list.jsx
+++ app/assets/javascripts/components/generic/button_list.jsx
@@ -1,10 +1,16 @@
+//contains two buttons which may be toggled on or off
+//    submit_button_text: "Submit"
+//    cancel_button_text: "Cancel"
+//    hide_submit_button: "true"
+//    hide_cancel_button: "true"
+
 var ButtonList = React.createClass({
     render: function() {
         var all_props = this.props;
         return (
             <div className="Button-overlay">
-                { this.props.hide_submit_button == 'true' ?  null : <SubmitButton {...all_props} /> }
-                 { this.props.hide_cancel_button == 'true' ?  null : <CancelButton {...all_props} /> }
+                { this.props.hide_submit_button === true ?  null : <SubmitButton {...all_props} /> }
+                 { this.props.hide_cancel_button === true ?  null : <CancelButton {...all_props} /> }
             </div>
         );
     }
