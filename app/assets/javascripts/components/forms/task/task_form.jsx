8329ef6b4eab75ecbe8e91a9a31354ae3b95f8f0
Sunny Vempati <sunnyvempati@gmail.com>
2015-03-15 15:35:49 -0600

    form abstraction

--- app/assets/javascripts/components/forms/task/task_form.jsx
+++ app/assets/javascripts/components/forms/task/task_form.jsx
@@ -9,70 +9,41 @@ var TaskForm = React.createClass({
       }
     };
   },
-
-  getInitialState: function() {
-    return {canSubmit: false};
-  },
-
-  enableButton: function () {
-    this.setState({
-      canSubmit: true
-    });
-  },
-  disableButton: function () {
-    this.setState({
-      canSubmit: false
-    });
-  },
-
   changeUrl: function() {
     location.href = '/tasks';
   },
-
-  hiddenFields: function() {
-    return (
-      <FormInput type="hidden" name={this.props.authParam} value={this.props.authToken} />
-    )
-  },
-
   render: function() {
-    var hidden_fields = this.props.disableForm ? "" : this.hiddenFields();
     return (
-      <div className="FormContainer">
-        <Formsy.Form  url={this.props.action}
-                      onSuccess={this.changeUrl}
-                      onValid={this.enableButton}
-                      onInvalid={this.disableButton}
-                      mapping={this.mapInputs}
-                      method={this.props.routeVerb}
-                       >
-            {hidden_fields}
-            <FormInput  name="name"
-                        autofocus="autofocus"
-                        placeholder="What is the name of your task?"
-                        type="text" label="name"
-                        value={this.props.model.name}
-                        disabled={this.props.disableForm}
-                        required/>
-            <FormInput  name="description"
-                        autofocus="off"
-                        placeholder="How would you describe this task?"
-                        type="text" label="description"
-                        value={this.props.model.description}
-                        disabled={this.props.disableForm} />
-            <FormInput  name="deadline" autofocus="off"
-                        placeholder="What is the deadline for this task? (DD/MM/YYYY)"
-                        type="datetime"
-                        label="deadline"
-                        value={this.props.model.deadline}
-                        disabled={this.props.disableForm} />
-            <ButtonList showButtonList={this.props.showButtonList}
-                        primaryButtonText={this.props.primaryButtonText}
-                        secondaryButtonVisible={this.props.secondaryButtonVisible}
-                        secondaryButtonHref={this.props.secondaryButtonHref}
-                        secondaryButtonText="Cancel"   />
-        </Formsy.Form>
-      </div>
+      <Form url={this.props.action}
+            mapping={this.mapInputs}
+            onSuccessUrl='/tasks'
+            routeVerb={this.props.routeVerb}
+            authToken={this.props.authToken}
+            primaryButtonText={this.props.primaryButtonText}
+            secondaryButtonVisible={this.props.secondaryButtonVisible}
+            secondaryButtonHref={this.props.secondaryButtonHref}
+            showButtonList={this.props.showButtonList}>
+
+        <FormInput  name="name"
+                    autofocus="autofocus"
+                    placeholder="What is the name of your task?"
+                    type="text" label="name"
+                    value={this.props.model.name}
+                    disabled={this.props.disableForm}
+                    required/>
+        <FormInput  name="description"
+                    autofocus="off"
+                    placeholder="How would you describe this task?"
+                    type="text" label="description"
+                    value={this.props.model.description}
+                    disabled={this.props.disableForm} />
+        <FormInput  name="deadline" autofocus="off"
+                    placeholder="What is the deadline for this task? (DD/MM/YYYY)"
+                    type="datetime"
+                    label="deadline"
+                    value={this.props.model.deadline}
+                    disabled={this.props.disableForm} />
+      </Form>
     );
   }
 });
