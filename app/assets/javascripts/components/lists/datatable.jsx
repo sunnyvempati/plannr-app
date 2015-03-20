b1712b96a8fe69582a4cf50aa84917ee2e788621
Justin Kobylarz <JustinKoby@gmail.com>
2015-03-20 11:56:04 -0500

    possibly broken stuff

--- app/assets/javascripts/components/lists/datatable.jsx
+++ app/assets/javascripts/components/lists/datatable.jsx
@@ -1,116 +1,124 @@
 var Datatable = React.createClass({
-  render: function () {
-    var thisTasks = this.props.data;
-    var props = this.props;
+    render: function () {
+        var thisDataArray = this.props.data;
+        var props = this.props;
 
-    var rows = thisTasks.map(function (task) {
-      return <DatatableRow task={task} displayFields={props.displayFields} addCheckboxColumn={props.addCheckboxColumn} addActionColumn={props.addActionColumn} />
-    });
+        var columns = buildColumnList(props.displayFields, props.addCheckboxColumn, props.addActionColumn);
 
-    return (
-      <table className='plannr_datatable hover less-compact'>
-        <DatatableHeader displayFields={props.displayFields} addCheckboxColumn={props.addCheckboxColumn} addActionColumn={props.addActionColumn}/>
-        <tbody>{rows}</tbody>
-      </table>
-    );
-  }
+        var rowDataArray = ConvertDataToRowDataArray(thisDataArray, columns);
+
+        var rows = thisDataArray.map(function (rowData) {
+            return <DatatableRow data={rowDataArray}  />
+        });
+
+        return (
+            <table className='plannr_datatable hover less-compact'>
+                <DatatableHeader columns={columns} />
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
 });
 
-var DatatableHeader = React.createClass({
+function ConvertDataToRowDataArray(data, columns) {
+    var retRowData = [];
+    $.each(data, function (index, value) {
+        var row = [];
+        $.each(columns, function (index2, value2) {
+            if (value2.left(6) === "plannr") {
+                if (value2 === "plannr_checkbox") {
+                    row.push("<input type='checkbox' />");
+                }
 
-  render: function () {
+                if (value2 === "plannr_actions") {
+                    row.push(<ActionDatatableCell hrefRoot={"/task"} id={value.id} />);
+                }
+            } else {
+                row.push(value[value2]);
+            }
+        });
+        retRowData.push(row);
+    });
 
-    var thisDisplayFields = this.props.displayFields;
-    var extraPreColumns = null;
-    var extraPostColumns = null;
-    if (this.props.addCheckboxColumn) {
-      extraPreColumns = (<th></th>);
+    return retRowData;
+}
+
+function buildColumnList(displayFields, showCheckboxColumn, showActionColumn) {
+    var retColumnList = [];
+
+    if (showCheckboxColumn) {
+        retColumnList.push({name: "plannr_checkbox", header: ""});
     }
 
-    if (this.props.addActionColumn) {
-      extraPostColumns = (<th>Actions</th>);
-    }
-
-    var headerCells = thisDisplayFields.map(function (field) {
-      return (
-        <DatatableHeaderCell data={field} />
-      );
+    $.each(displayFields, function (index, value) {
+        retColumnList.push({name: value, header: value})
     })
 
-    return (<thead>
-      <tr>{extraPreColumns} {headerCells} {extraPostColumns}</tr>
-    </thead>);
-  }
+    if (showActionColumn) {
+        retColumnList.push({name: "plannr_action", header: "Actions"});
+    }
+
+    return retColumnList;
+}
+
+var DatatableHeader = React.createClass({
+    render: function () {
+        var thisColumns = this.props.columns;
+
+        var headerCells = [];
+        $.each(thisColumns, function (index, value) {
+            headerCells.push(<DatatableHeaderCell data={value.header} />);
+        });
+
+        return (<thead>
+            <tr>{headerCells}</tr>
+        </thead>);
+    }
 
 });
 
 
 var DatatableRow = React.createClass({
-  render: function () {
+    render: function () {
+        var thisData = this.props.data;
 
-    var thisTask = this.props.task;
-    var thisDisplayFields = this.props.displayFields;
+        var retX = [];
+        $.each(thisData, function (index, value) {
+            retX.push(<td>{value}</td>);
+        });
 
-    var keysToDisplay = [];
-    if (this.props.addCheckboxColumn) {
-      keysToDisplay.push('plannr_checkbox');
+
+        return (
+            <tr>{retX}</tr>
+        );
     }
-    for (var key in thisTask) {
-      if (thisTask.hasOwnProperty(key)) {
-        if ($.inArray(key, thisDisplayFields) >= 0) {
-          keysToDisplay.push(key);
-        }
-      }
-    }
-    if (this.props.addActionColumn) {
-      keysToDisplay.push('plannr_actions');
-    }
-
-    var retCells = keysToDisplay.map(function (key) {
-      var retData;
-      var checkboxHtml = (<input type='checkbox' />);
-      if (key === 'plannr_checkbox') {
-        retData = (<DatatableCell data={checkboxHtml} />);
-      }
-      else if (key === 'plannr_actions') {
-        retData = (<ActionDatatableCell id={thisTask.id} />);
-      }
-      else {
-        retData = (<DatatableCell data={thisTask[key]} />);
-      }
-
-      return (
-        retData
-      );
-    });
-
-    return (
-      <tr>{retCells}</tr>
-    );
-  }
 });
 
 var DatatableCell = React.createClass({
-  render: function () {
-    return (<td>{this.props.data}</td>);
-  }
+    render: function () {
+        return (<td>{this.props.data}</td>);
+    }
 });
 
 var DatatableHeaderCell = React.createClass({
-  render: function () {
-    return (<th>{this.props.data}</th>);
-  }
+    render: function () {
+        return (<th>{this.props.data}</th>);
+    }
 });
 
 var ActionDatatableCell = React.createClass({
-  render: function () {
-    var showHref = "/tasks/" + this.props.id;
-    var editHref = "/tasks/" + this.props.id + "/edit";
+    render: function () {
+        var thisHrefRoot = this.props.hrefRoot;
+        var thisId = this.props.id;
+        var showHref = thisHrefRoot + "/" + thisId;
+        var editHref = thisHrefRoot + "/" + thisId + "/edit";
 
-    return(<td>
-      <a href={showHref}>Show</a> <a href={editHref}>Edit</a> <a data-confirm='Are you sure?' rel='nofollow' data-method='delete' href={showHref}>Destroy</a>
-    </td>);
-  }
+        return (<span>
+            <a href={showHref}>Show</a>
+            <a href={editHref}>Edit</a>
+            <a data-confirm='Are you sure?' rel='nofollow' data-method='delete' href={showHref}>Destroy</a>
+        </span>);
+    }
 });
 
 
