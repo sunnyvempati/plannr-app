3dfe11bc7c975c2b887561f327231f3c386572b0
JustinK322 <justin@yourplannr.com>
2015-03-25 09:55:32 -0600

    defect: fixed vendor save

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -48,7 +48,7 @@ class VendorsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def vendor_params
-    params.require(:vendor).permit(:name, :description, :deadline)
+    params.require(:vendor).permit(:name, :location, :phone, :primary_contact)
   end
 
 end
