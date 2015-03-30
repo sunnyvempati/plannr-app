33b947cc0b8ecafdfac59a3031ca1a1f235b708d
JustinK322 <justin@yourplannr.com>
2015-03-30 17:16:03 -0600

    rails g resource contact ...

---
+++ db/migrate/20150330231529_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :company
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/contacts.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :contact do
+    name "MyString"
+email "MyString"
+phone "MyString"
+company "MyString"
+description "MyText"
+  end
+
+end
