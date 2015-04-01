c593db1038139f9923b92e20095423385f748267
JustinK322 <justin@yourplannr.com>
2015-03-31 21:28:29 -0600

    vendor cruel pages without validation

---
+++ app/assets/javascripts/components/forms/vendor/vendor_form.jsx
@@ -0,0 +1,77 @@
+var VendorForm = React.createClass({
+  mapInputs: function(inputs) {
+    return {
+      'authenticity_token': inputs.authenticity_token,
+      'vendor': {
+        'name': inputs.name,
+        'location': inputs.location,
+        'phone': inputs.phone,
+        'primary_contact': inputs.primary_contact
+
+      }
+    };
+  },
+  changeUrl: function() {
+    location.href = '/vendors';
+  },
+  render: function() {
+    return (
+      <div className="FormContainer--leftAligned">
+        <Form url={this.props.action}
+          mapping={this.mapInputs}
+          onSuccessUrl='/vendors'
+          routeVerb={this.props.routeVerb}
+          authToken={this.props.authToken}
+          primaryButtonText={this.props.primaryButtonText}
+          secondaryButtonVisible={this.props.secondaryButtonVisible}
+          secondaryButtonHref={this.props.secondaryButtonHref}
+          showButtonList={this.props.showButtonList}>
+
+          <FormInput
+            id="vendor_name"
+            name="name"
+            autofocus="autofocus"
+            placeholder="What is the name of your vendor?"
+            type="text" 
+            label="name*"
+            value={this.props.model.name}
+            disabled={this.props.disableForm}
+            required
+          />
+          <FormInput
+            id="vendor_location"
+            name="location"
+            placeholder="What is the location of your vendor?"
+            type="text" 
+            label="vendor_location"
+            value={this.props.model.location}
+            disabled={this.props.disableForm}
+          />
+          <FormInput
+            id="vendor_phone"
+            name="phone"
+            placeholder="What is the phone of your vendor?"
+            type="text"
+            label="phone"
+            value={this.props.model.phone}
+            disabled={this.props.disableForm}
+          />
+          <FormInput
+            id="vendor_primary_contact"
+            name="primary_contact"
+            placeholder="What is the primary_contact of your vendor?"
+            type="text"
+            label="primary_contact"
+            value={this.props.model.primary_contact}
+            disabled={this.props.disableForm}
+          />
+
+        </Form>
+
+        <a href={this.props.hrefRoot }>List</a>|
+        <a href={this.props.hrefRoot + "/" + this.props.model.id + "/edit" }>Edit</a>|
+        <a href={this.props.hrefRoot + "/" + this.props.model.id  }>Show</a>
+      </div>
+    );
+  }
+});
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,55 @@
 class VendorsController < ApplicationController
+  layout 'main'
+  before_action :authenticate_user
+  before_action :set_vendor, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @vendors = Vendor.all
+    @header = "Vendors"
+  end
+
+  def show
+    @header = "Vendor"
+  end
+
+  def new
+    @vendor = Vendor.new
+    @header = "Create Vendor"
+  end
+
+  def edit
+    @header = "Edit Vendor"
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+    render_entity @vendor
+  end
+
+  def update
+    @vendor.assign_attributes(vendor_params)
+    render_entity @vendor
+  end
+
+  def destroy
+    @vendor.destroy
+    respond_to do |format|
+      format.html { redirect_to vendors_url, notice: 'Vendor was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_vendor
+    @vendor = Vendor.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def vendor_params
+    params.require(:vendor).permit(:name, :location, :phone, :primary_contact)
+  end
+
 end
+
