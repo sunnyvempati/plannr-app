09a22a37f5cb56afdaf994382c729e643df17f7a
JustinK322 <justin@yourplannr.com>
2015-04-02 08:50:47 -0600

    allow blank email address

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,7 @@ class Contact < ActiveRecord::Base
 
   validates :name, presence: true
   validate :contact_contact_type
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'is an invalid email addressexit'
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'is an invalid email address', :allow_blank => true
 
   def contact_contact_type
     if contact_type && (contact_type != CLIENT  && contact_type != VENDOR)
