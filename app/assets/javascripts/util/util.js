157826f699093454d19519f762e8d7517c8e405c
JustinK322 <justin@yourplannr.com>
2015-04-01 18:50:46 -0600

    refactor

--- app/assets/javascripts/util/util.js
+++ app/assets/javascripts/util/util.js
@@ -1,23 +1,20 @@
 var util = (function () {
-
-  //convert ISO date (Bing it) to US date (MM/DD/YYYY)
-  function isoDateToUsFormat(isoDate ) {
-    var retDate;
-    var separator = "/";
-    if (!!isoDate) {
-      var d = new Date(isoDate);
-      var mm = d.getMonth() + 1;
-      var dd = d.getDate() + 1;
-      var yyyy = d.getFullYear();
-      retDate = "".concat(mm,separator,dd,separator,yyyy);
-    }else {
-      retDate = "";
-    }
-    return retDate;
-  }
-
   return {
-    isoDateToUsFormat: isoDateToUsFormat
+    //convert ISO date (Bing it) to US date (MM/DD/YYYY)
+    isoDateToUsFormat: function (isoDate ) {
+      var retDate;
+      var separator = "/";
+      if (!!isoDate) {
+        var d = new Date(isoDate);
+        var mm = d.getMonth() + 1;
+        var dd = d.getDate() + 1;
+        var yyyy = d.getFullYear();
+        retDate = "".concat(mm,separator,dd,separator,yyyy);
+      }else {
+        retDate = "";
+      }
+      return retDate;
+    }
   }
 
 }());
