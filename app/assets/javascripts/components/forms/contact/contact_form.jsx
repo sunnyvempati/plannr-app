e9af98df69c8148f9078bece7b05478f91d26f54
JustinK322 <justin@yourplannr.com>
2015-04-03 11:37:43 -0600

    switched to single quotes for consistency

--- app/assets/javascripts/components/forms/contact/contact_form.jsx
+++ app/assets/javascripts/components/forms/contact/contact_form.jsx
@@ -1,5 +1,5 @@
 var ContactForm = React.createClass({
-  hrefRoot: "/contacts",
+  hrefRoot: '/contacts',
   contactTypeOptions: [ {value: '1', text: 'Client'}, {value: '2', text: 'Vendor'}],
   mapInputs: function (inputs) {
     return {
@@ -19,7 +19,7 @@ var ContactForm = React.createClass({
   },
   render: function () {
     return (
-      <div className="FormContainer--leftAligned">
+      <div className='FormContainer--leftAligned'>
         <Form url={this.props.action}
           mapping={this.mapInputs}
           onSuccess={this.changeUrl}
@@ -32,59 +32,59 @@ var ContactForm = React.createClass({
           id='contact_form'>
 
           <FormInput
-            id="contact_name"
-            name="name"
-            autofocus="autofocus"
-            placeholder="What is the name of your contact?"
-            type="text"
-            label="name*"
+            id='contact_name'
+            name='name'
+            autofocus='autofocus'
+            placeholder='What is the name of your contact?'
+            type='text'
+            label='name*'
             value={this.props.model.name}
             disabled={this.props.disableForm}
             required
           />
           <SelectInput
-            id="contact_contact_type"
-            name="contact_type"
-            className="SelectInput"
-            label="type*"
+            id='contact_contact_type'
+            name='contact_type'
+            className='SelectInput'
+            label='type*'
             options={this.contactTypeOptions}
             value={this.props.model.contact_type}
             disabled={this.props.disableForm}
             required
           />
           <FormInput
-            id="contact_email"
-            name="email"
-            placeholder="What is the email of your contact?"
-            type="text"
-            label="email"
+            id='contact_email'
+            name='email'
+            placeholder='What is the email of your contact?'
+            type='text'
+            label='email'
             value={this.props.model.email}
             disabled={this.props.disableForm}
           />
           <FormInput
-            id="contact_phone"
-            name="phone"
-            placeholder="What is the phone of your contact?"
-            type="tel"
-            label="phone"
+            id='contact_phone'
+            name='phone'
+            placeholder='What is the phone of your contact?'
+            type='tel'
+            label='phone'
             value={this.props.model.phone}
             disabled={this.props.disableForm}
           />
           <FormInput
-            id="contact_company"
-            name="company"
-            placeholder="What is the company of your contact?"
-            type="text"
-            label="company"
+            id='contact_company'
+            name='company'
+            placeholder='What is the company of your contact?'
+            type='text'
+            label='company'
             value={this.props.model.company}
             disabled={this.props.disableForm}
           />
           <TextAreaInput
-            name="description"
-            id="contact_description"
-            className="TextAreaInput"
-            label="description"
-            placeholder="What else do you need to know?"
+            name='description'
+            id='contact_description'
+            className='TextAreaInput'
+            label='description'
+            placeholder='What else do you need to know?'
             value={this.props.model.description}
             disabled={this.props.disableForm}
             formId='contact_form'
@@ -94,9 +94,9 @@ var ContactForm = React.createClass({
 
         <a href={this.hrefRoot }>List</a>
         |
-        <a href={this.hrefRoot + "/" + this.props.model.id + "/edit" }>Edit</a>
+        <a href={this.hrefRoot + '/' + this.props.model.id + '/edit' }>Edit</a>
         |
-        <a href={this.hrefRoot + "/" + this.props.model.id  }>Show</a>
+        <a href={this.hrefRoot + '/' + this.props.model.id  }>Show</a>
       </div>
     );
   }
