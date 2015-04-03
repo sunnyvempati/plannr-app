cf472779494166887209326a6e303b9e18062e40
JustinK322 <justin@yourplannr.com>
2015-04-03 12:24:10 -0600

    double quotes to single quotes for consistency

--- app/assets/javascripts/components/forms/vendor/vendor_form.jsx
+++ app/assets/javascripts/components/forms/vendor/vendor_form.jsx
@@ -1,5 +1,5 @@
 var VendorForm = React.createClass({
-  hrefRoot: "/vendors",
+  hrefRoot: '/vendors',
   mapInputs: function(inputs) {
     return {
       'authenticity_token': inputs.authenticity_token,
@@ -16,7 +16,7 @@ var VendorForm = React.createClass({
   },
   render: function() {
     return (
-      <div className="FormContainer--leftAligned">
+      <div className='FormContainer--leftAligned'>
         <Form url={this.props.action}
           mapping={this.mapInputs}
           onSuccess={this.changeUrl}
@@ -28,40 +28,40 @@ var VendorForm = React.createClass({
           showButtonList={this.props.showButtonList}>
 
           <FormInput
-            id="vendor_name"
-            name="name"
-            autofocus="autofocus"
-            placeholder="What is the name of your vendor?"
-            type="text" 
-            label="name*"
+            id='vendor_name'
+            name='name'
+            autofocus='autofocus'
+            placeholder='What is the name of your vendor?'
+            type='text' 
+            label='name*'
             value={this.props.model.name}
             disabled={this.props.disableForm}
             required
           />
           <FormInput
-            id="vendor_location"
-            name="location"
-            placeholder="What is the location of your vendor?"
-            type="text" 
-            label="vendor_location"
+            id='vendor_location'
+            name='location'
+            placeholder='What is the location of your vendor?'
+            type='text' 
+            label='vendor_location'
             value={this.props.model.location}
             disabled={this.props.disableForm}
           />
           <FormInput
-            id="vendor_phone"
-            name="phone"
-            placeholder="What is the phone of your vendor?"
-            type="text"
-            label="phone"
+            id='vendor_phone'
+            name='phone'
+            placeholder='What is the phone of your vendor?'
+            type='text'
+            label='phone'
             value={this.props.model.phone}
             disabled={this.props.disableForm}
           />
           <FormInput
-            id="vendor_primary_contact"
-            name="primary_contact"
-            placeholder="Who is the primary_contact of your vendor?"
-            type="text"
-            label="primary_contact"
+            id='vendor_primary_contact'
+            name='primary_contact'
+            placeholder='Who is the primary_contact of your vendor?'
+            type='text'
+            label='primary_contact'
             value={this.props.model.primary_contact}
             disabled={this.props.disableForm}
           />
@@ -69,8 +69,8 @@ var VendorForm = React.createClass({
         </Form>
 
         <a href={this.hrefRoot }>List</a>|
-        <a href={this.hrefRoot + "/" + this.props.model.id + "/edit" }>Edit</a>|
-        <a href={this.hrefRoot + "/" + this.props.model.id  }>Show</a>
+        <a href={this.hrefRoot + '/' + this.props.model.id + '/edit' }>Edit</a>|
+        <a href={this.hrefRoot + '/' + this.props.model.id  }>Show</a>
       </div>
     );
   }
