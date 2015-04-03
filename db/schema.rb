28fb6aaa20e2f7c454baf09e0f61312d343a7340
JustinK322 <justin@yourplannr.com>
2015-04-03 12:48:20 -0600

    schema updated, vendor craziness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402212629) do
+ActiveRecord::Schema.define(version: 20150313002912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,13 +54,6 @@ ActiveRecord::Schema.define(version: 20150402212629) do
     t.uuid     "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "table_vendors", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "phone"
-    t.string "primary_contact"
   end
 
   create_table "tasks", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
