7511d6f0e8c59fc129d16413677f7f7887d2ab95
Sunny Vempati <sunnyvempati@gmail.com>
2015-04-03 14:48:29 -0600

    removed port

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -10,8 +10,7 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = {
-    :host => 'staging.yourplannr.com',
-    :port => 3000
+    :host => 'staging.yourplannr.com'
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
