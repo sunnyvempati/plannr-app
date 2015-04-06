9c59eac1e8c52f49323c0a1693fcda131075fd69
JustinK322 <justin@yourplannr.com>
2015-04-06 14:58:05 -0600

    included param helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   respond_to :html, :json
   include ResponseHelpers
   include DateHelpers
+  include ParamHelpers
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
