e573f72eab75aed38ef14dd529a8980895d3520d
JustinK322 <justin@yourplannr.com>
2015-04-06 16:19:13 -0600

    removed owner_id from list of permitted params because it isn't needed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -50,7 +50,7 @@ class ContactsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def contact_params
-    params.require(:contact).permit(:name, :email, :contact_type, :phone, :contact_company, :description, :owner_id)
+    params.require(:contact).permit(:name, :email, :contact_type, :phone, :contact_company, :description)
   end
 
 end
