427557d6f33e6a3a406de9f6ac4b434eab094748
JustinK322 <justin@yourplannr.com>
2015-04-06 12:46:15 -0600

    changed error message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,6 +7,7 @@ class Contact < ActiveRecord::Base
                       :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
                       :message => 'is an invalid email address',
                       :allow_blank => true
+  validates_uniqueness_of :email, scope: :company_id, message: 'must be unique to your company'
 
   def contact_type
     read_attribute(:contact_type).to_s
