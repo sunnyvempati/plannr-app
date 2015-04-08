be0e2cd7a4df354c69c47415a21708fa06e52cde
JustinK322 <justin@yourplannr.com>
2015-04-08 17:54:25 -0600

    allow blank and nil emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,6 @@ class Contact < ActiveRecord::Base
   
   enum contact_type: {client: 1, vendor: 2}
 
- scope :name_or_email_like, ->(search_condition) {      where('contacts.name LIKE ? OR contacts.email LIKE ?', search_condition, search_condition)  }
 
 
   validates :name,
@@ -21,7 +20,7 @@ class Contact < ActiveRecord::Base
                       :with => %r{\A(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?\z},
                       :message => 'must be a phone number in [1-]999-999-9999 [x9999] format',
                       :allow_blank => true
-  validates_uniqueness_to_tenant :email,  message: 'this email already exists in your company'
+  validates_uniqueness_to_tenant :email, allow_blank: true, allow_nil: true,   message: 'this email already exists in your company'
 
   def contact_type
     read_attribute(:contact_type).to_s
