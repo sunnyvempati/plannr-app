8776d3a99cd7d5d5f0b542e7d101a03fe4cd7dc5
JustinK322 <justin@yourplannr.com>
2015-04-08 17:56:50 -0600

    create contact when no id is passed in

--- app/controllers/event_contacts_controller.rb
+++ app/controllers/event_contacts_controller.rb
@@ -16,7 +16,17 @@ class EventContactsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_event_contact
     @event = Event.find_by_id(params[:event_id])
-    @contact = Contact.find_by_id(params[:contact_id])
+    if params[:contact_id] == "-1"
+      if params[:searchText].index(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i) 
+        # if email address, use for name and email
+        @contact = Contact.create(name: params[:searchText], email:  params[:searchText])       
+      else 
+        # if not email address, use for just name
+        @contact = Contact.create(name: params[:searchText])
+      end
+    else
+      @contact = Contact.find_by_id(params[:contact_id])
+    end
   end
 
 end
