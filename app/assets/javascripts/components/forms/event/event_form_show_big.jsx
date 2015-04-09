cc37bb6242f72ae690cc057acde98410e663beb8
JustinK322 <justin@yourplannr.com>
2015-04-09 17:48:45 -0600

    formatting

--- app/assets/javascripts/components/forms/event/event_form_show_big.jsx
+++ app/assets/javascripts/components/forms/event/event_form_show_big.jsx
@@ -6,18 +6,19 @@ var EventFormShowBig = React.createClass({
     return (
       <div>
 
-      <ContactAssociationEditor 
-        associatedObjectId={this.props.model.id} 
-        retrieveAssociatedDataUrl={"/events/" + this.props.model.id + "/associated_contacts"}
-        retrieveUnassociatedDataUrl={"/events/" + this.props.model.id + "/unassociated_contacts"}
+        <ContactAssociationEditor 
+          associatedObjectId={this.props.model.id} 
+          retrieveAssociatedDataUrl={"/events/" + this.props.model.id + "/associated_contacts"}
+          retrieveUnassociatedDataUrl={"/events/" + this.props.model.id + "/unassociated_contacts"}
+          />
+          
+        <EventForm
+          action= {action}
+          model={this.props.model}
+          disableForm={true}
+          showButtonList={false}
+          notice={this.props.notice}
         />
-      <EventForm
-        action= {action}
-        model={this.props.model}
-        disableForm={true}
-        showButtonList={false}
-        notice={this.props.notice}
-      />
 
       </div>
       );
