93700d32cb91dc1044fa3f7c15818e6349d14610
JustinK322 <justin@yourplannr.com>
2015-04-09 17:47:55 -0600

    moved class to separate file

--- app/assets/javascripts/components/forms/contact/contact_association_list.jsx
+++ app/assets/javascripts/components/forms/contact/contact_association_list.jsx
@@ -31,29 +31,3 @@ var ContactAssociationList = React.createClass({
   }
 
 });
-
-var EventContactsLinkAsync = React.createClass({
-  onClickAsync: function() {
-    var _this = this;
-    $.ajax({
-      url: "/event_contacts?contact_id=" + _this.props.contactId + "&event_id=" + _this.props.associatedObjectId,
-      dataType: "json",
-      type: _this.props.type,
-      success: function ( data ) {
-        _this.props.onSuccessCallback();
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-              //TODO: better error handling
-              throw "jqXHR=" + jqXHR + "; textStatus=" + textStatus + "; errorThrown" + errorThrown;
-            }
-          });
-  },
-
-  render: function() {
-    return (
-      <a rel="nofollow" href='#' onClick={this.onClickAsync}>{this.props.text}</a>
-      );
-  }
-});
-
-
