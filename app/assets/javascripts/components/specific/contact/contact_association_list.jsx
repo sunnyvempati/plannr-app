194b6f31d76ccd004e3aca90083f9bf5d9476287
JustinK322 <justin@yourplannr.com>
2015-04-09 18:00:35 -0600

    check propTypes

--- app/assets/javascripts/components/specific/contact/contact_association_list.jsx
+++ app/assets/javascripts/components/specific/contact/contact_association_list.jsx
@@ -1,4 +1,9 @@
 var ContactAssociationList = React.createClass({
+  propTypes: {
+    associated: React.PropTypes.bool.isRequired,
+    associatedObjectId: React.PropTypes.string.isRequired,
+    onSuccessCallback: React.PropTypes.func.isRequired
+  },
   render: function () {
     var createItem = function (item, index, isAssociated, associatedObjectId, onSuccessCallback) {
       if (isAssociated) {
