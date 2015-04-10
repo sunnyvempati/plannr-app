e149a6439300dd8bd8fff5bf130413ccd9d0d961
JustinK322 <justin@yourplannr.com>
2015-04-10 16:41:17 -0600

    reverted the destruction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,50 @@
-require 'faker'
-
-c = Company.create(name: Faker::Company.name)
-
-20.times do
-	u = User.create(email: Faker::Internet.email, password: "plannr12", password_confirmation: "plannr12", company: c)
-	Profile.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, user: u)
-end
-
+plannr_company = Company.create(name: "Plannr")   
+puts "Successfully created company for Plannr" if plannr_company   
+    
+other_company = Company.create(name: "Genesis")   
+puts "Successfully created company for Genesis" if other_company    
+    
+sunny_user = User.create(email: 'sunny@yourplannr.com', password: 'plannr12', password_confirmation: 'plannr12', company: plannr_company)    
+puts "Successfully created account for Sunny" if sunny_user    
+  
+justin_user = User.create(email: 'justin@yourplannr.com', password: 'plannr12', password_confirmation: 'plannr12', company: plannr_company)    
+puts "Successfully created account for Justin" if justin_user    
+   
+josh_user = User.create(email: 'josh@yourplannr.com', password: 'plannr12', password_confirmation: 'plannr12', company: plannr_company)    
+puts "Successfully created account for Josh" if josh_user    
+   
+phil_user = User.create(email: 'phil@genesis.com', password: 'plannr12', password_confirmation: 'plannr12', company: other_company)   
+puts "Successfully created account for Phil" if phil_user    
+   
+sunny_profile = Profile.create(user: sunny_user, first_name: "Sunny", last_name: "Vempati")    
+puts "Successfully created profile for Sunny" if sunny_profile   
+   
+justin_profile = Profile.create(user: justin_user, first_name: "Justin", last_name: "Kobylarz")    
+puts "Successfully created profile for Justin" if justin_profile   
+   
+josh_profile = Profile.create(user: josh_user, first_name: "Josh", last_name: "Haas")    
+puts "Successfully created profile for Josh" if josh_profile   
+   
+phil_profile = Profile.create(user: josh_user, first_name: "Phil", last_name: "Collins")   
+puts "Successfully created profile for Phil" if phil_profile   
+   
+Event.create(company: plannr_company,    
+            name: "26th Birthday Bash",    
+            client_name: Faker::Name.name,   
+            start_date: Faker::Date.forward(45),   
+            budget: 2500,    
+            location: "Chicago, IL")   
+   
+puts "Created 26th Birthday Bash"    
+   
+Event.create(company: plannr_company,    
+            name: "Dinner for Schmucks",   
+            client_name: Faker::Name.name,   
+            start_date: Faker::Date.forward(63),   
+            budget: 1150,    
+            location: "Chicago, IL")   
+   
+puts "Created Dinner for Schmucks"
 
 
 sam_contact = Contact.create(name: "Sam Doe", email: "sam@doe.com", company: plannr_company)
@@ -20,6 +58,3 @@ puts "Successfully created contact for Mike" if mike_contact
 
 john_contact = Contact.create(name: "John Other", email: "john@other.com", company: other_company)
 puts "Successfully created contact for John" if john_contact 
-
-
-
