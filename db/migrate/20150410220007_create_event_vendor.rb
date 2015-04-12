a539505108cab3ae0b80896062fa46f461de7780
JustinK322 <justin@yourplannr.com>
2015-04-12 11:13:42 -0600

    removed ids from EventContacts and EventVendors because I figured out how to get the delete to work without them - part 2

--- db/migrate/20150410220007_create_event_vendor.rb
+++ db/migrate/20150410220007_create_event_vendor.rb
@@ -1,6 +1,6 @@
 class CreateEventVendor < ActiveRecord::Migration
   def change
-    create_table :event_vendors, id: :uuid do |t|
+    create_table :event_vendors, id: false do |t|
       t.uuid :vendor_id, index: true
       t.uuid :event_id, index: true    
     end
