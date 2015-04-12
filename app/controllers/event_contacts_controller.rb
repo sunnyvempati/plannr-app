0faa391110be1f7b1a0a67a93812520f8118463a
JustinK322 <justin@yourplannr.com>
2015-04-12 10:53:47 -0600

    fixed so it only removes association and doesn't delete contacts

--- app/controllers/event_contacts_controller.rb
+++ app/controllers/event_contacts_controller.rb
@@ -13,13 +13,14 @@ class EventContactsController < ApplicationController
   def destroy
     event_contact = EventContact.where(event_id: event_contact_params[:event_id],
                                        contact_id: event_contact_params[:contact_id]).first
-    render_success event_contact.destroy
+    render_success event_contact.delete
   end
 
   def mass_destroy
-    event = Event.find(mass_destroy_params[:event_id])
-    contacts_to_delete = event.contacts.where(id: mass_destroy_params[:contact_ids])
-    render_success contacts_to_delete.delete_all
+    event_contacts =   EventContact.where(event_id: mass_destroy_params[:event_id],
+                                       contact_id: mass_destroy_params[:contact_ids])
+
+    render_success event_contacts.delete_all
   end
 
   private
