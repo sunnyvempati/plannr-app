3143a46bea8ef69b8e4a2da31c1de217e086ca17
JustinK322 <justin@yourplannr.com>
2015-04-13 12:33:32 -0600

    defect: now using dup to copy params

--- app/controllers/concerns/param_helpers.rb
+++ app/controllers/concerns/param_helpers.rb
@@ -8,7 +8,7 @@ module ParamHelpers
 
   def convert_date_params_to_date_type(entity_params, date_field_names)
     # change date params from string to date
-    modified_entity_params = entity_params
+    modified_entity_params = entity_params.dup
     date_field_names.each do |date_field_name|
       modified_entity_params.except!(date_field_name)
       modified_entity_params.merge!(date_field_name => convert_us_formatted_string_to_date_type(entity_params[date_field_name]))
