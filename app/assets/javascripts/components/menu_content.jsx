cc5c4f2b2cb50412ee077ad7135b05ca814d1000
JustinK322 <justin@yourplannr.com>
2015-04-14 10:43:17 -0600

    added keys to menu items to get rid of React chrome console errors

--- app/assets/javascripts/components/menu_content.jsx
+++ app/assets/javascripts/components/menu_content.jsx
@@ -1,16 +1,16 @@
 var MenuContent = React.createClass({
   render: function() {
     var menu_items = [
-        {name: "Events", icon_class: "fa fa-tachometer", href: "/"},
-        {name: "Tasks", icon_class: "fa fa-check", href: "/tasks"},
-        {name: "Messages", icon_class: "fa fa-comments", href: "/messages"},
-        {name: "Contacts", icon_class: "fa fa-book", href: "/contacts"},
-        {name: "Vendors", icon_class: "fa fa-truck", href: "/vendors"}
+        {name: "Events", icon_class: "fa fa-tachometer", href: "/", key: '1'},
+        {name: "Tasks", icon_class: "fa fa-check", href: "/tasks", key: '2'},
+        {name: "Messages", icon_class: "fa fa-comments", href: "/messages", key: '3'},
+        {name: "Contacts", icon_class: "fa fa-book", href: "/contacts", key: '4'},
+        {name: "Vendors", icon_class: "fa fa-truck", href: "/vendors", key: '5'}
         ];
     var active_path = this.props.active_path;
     var menu_items = menu_items.map(function(menu_item) {
       return (
-        <MenuItem item={menu_item} isSelected={menu_item.href == active_path} />
+        <MenuItem key={menu_item.key} item={menu_item} isSelected={menu_item.href == active_path} />
       );
     });
     return (
