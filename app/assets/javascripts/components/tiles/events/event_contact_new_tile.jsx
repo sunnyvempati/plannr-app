c0941296df03f256dfcd68559e32e737a4a78e2b
JustinK322 <justin@yourplannr.com>
2015-04-15 16:54:22 -0500

    added

---
+++ app/assets/javascripts/components/tiles/events/event_contact_new_tile.jsx
@@ -0,0 +1,19 @@
+var EventContactNewTile = React.createClass({
+  propTypes: {
+    authToken: React.PropTypes.string.isRequired
+  },
+  getInitialState: function() {
+    return {
+      model: null
+    };
+  },
+  render: function() {
+    var model = {event_id: this.props.eventId};
+    return (
+      <div className="EventContactNewTile">
+        <Link to="tileAll">Go back to home</Link>
+        <ContactFormNew model={model} authToken={this.props.authToken}/>
+      </div>
+    );
+  }
+});
