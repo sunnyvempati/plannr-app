091ee2fb2ae2015e877b18a4c1be2deb6413fa62
JustinK322 <justin@yourplannr.com>
2015-04-30 21:25:29 -0600

    slightly better factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :user do
+    email Faker::Internet.email
+    password "plannr12"
+    password_confirmation "plannr12"
+    company
+
+    after(:create) do |user|
+      create(:profile, user: user)
+    end
+
+  end
+
+end
