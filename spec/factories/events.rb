2fbd3054191c9eb7b2b7a3f4ddbc9e7729a9e8e7
JustinK322 <justin@yourplannr.com>
2015-05-05 17:27:32 -0600

    field is now client_id

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory 'event' do
     name { Faker::Company.catch_phrase }
-    client_name { Faker::Name.name }
     start_date { Date.today + 10.days }
     end_date { start_date + 1.days }
     budget { Faker::Number.number(5) }
