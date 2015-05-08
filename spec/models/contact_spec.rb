1501b7055e1b88d8670a8ee0d69002f78080e617
JustinK322 <justin@yourplannr.com>
2015-05-07 20:23:33 -0600

    condensed tests

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -16,45 +16,25 @@ RSpec.describe Contact, type: :model do
 
     describe " Email Validations" do
 
-      it "is valid with nil email" do
-        expect(FactoryGirl.build(:contact, email: nil)).to be_valid
+      [nil, 'billy@gmail.com', 'billy@gmail.net', 'billy.stuff@gmail.net'].each do |valid_email|
+        it "is valid with #{valid_email} email" do
+          expect(FactoryGirl.build(:contact, email: valid_email)).to be_valid
+        end
       end
 
-      it "is valid with billy@gmail.com email" do
-       expect(FactoryGirl.build(:contact, email:'billy@gmail.com')).to be_valid
-     end
-
-     it "is valid with billy.stuff@gmail.com email" do
-       expect(FactoryGirl.build(:contact, email:'billy.stuff@gmail.com')).to be_valid
-     end
-
-     it "is valid with billy.stuff@gmail.net email" do
-       expect(FactoryGirl.build(:contact, email:'billy.stuff@gmail.net')).to be_valid
-     end
-
-     it "is invalid without .com at end of email" do
-       expect(FactoryGirl.build(:contact, email:'billy@gmail')).to_not be_valid
-     end
+      it "is invalid without .com at end of email" do
+        expect(FactoryGirl.build(:contact, email:'billy@gmail')).to_not be_valid
+      end
 
    end
 
 
    describe " Phone Validations " do
 
-    it "is valid with nil phone" do
-      expect(FactoryGirl.build(:contact, phone: nil)).to be_valid
-    end
-
-    it "is valid with phone for Amcore Bank Time and Temp" do
-      expect(FactoryGirl.build(:contact, phone: '815-968-2311')).to be_valid
-    end
-
-    it "is valid with phone for Amcore Bank Time and Temp without dashes" do
-      expect(FactoryGirl.build(:contact, phone: '8159682311')).to be_valid
-    end
-
-    it "is valid with phone for Amcore Bank Time and Temp with 1" do
-      expect(FactoryGirl.build(:contact, phone: '1-815-968-2311')).to be_valid
+    [nil, '815-968-2311', '8159682311', '1-815-968-2311'].each do |valid_phone|
+      it "is valid with #{valid_phone} phone" do
+        expect(FactoryGirl.build(:contact, phone: valid_phone)).to be_valid
+      end
     end
 
     it "is invalid with a 9 digit number" do
