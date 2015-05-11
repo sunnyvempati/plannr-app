7252810960eec3ffcf21295d0198fc8662b5c3b4
Sunny Vempati <sunnyvempati@gmail.com>
2015-05-11 14:22:41 -0600

    vendor auto create

--- app/assets/javascripts/components/autocomplete/event_vendor_autocomplete.jsx
+++ app/assets/javascripts/components/autocomplete/event_vendor_autocomplete.jsx
@@ -1,4 +1,5 @@
 var EventVendorAutocomplete = React.createClass({
+  mixins: [AutocompleteBoldItem, AutocompleteRenderNew],
   propTypes: {
     onAssociation: React.PropTypes.func.isRequired
   },
@@ -9,11 +10,16 @@ var EventVendorAutocomplete = React.createClass({
   },
   retrieveVendors: function(term) {
     $.get("search_other_vendors", {search: {text: term}},  function(result) {
-      this.setState({vendors: result.vendors})
+      var vendors = result.vendors;
+      if (vendors.length == 0) {
+        vendors.push(this.getNewItem("vendor"));
+      }
+      this.setState({vendors: vendors})
     }.bind(this));
   },
-  addVendorToEvent: function(vendor) {
-    var payload = {event_vendor: {vendor_id: vendor.id}};
+  addVendorToEvent: function(vendor, term) {
+    var eventVendorPayload = vendor.id == -1 ? {name: term} : {vendor_id: vendor.id};
+    var payload = {event_vendor: eventVendorPayload};
     $.post("vendors", payload, function(result) {
       this.props.onAssociation(result.event_vendor_with_vendor);
     }.bind(this))
@@ -24,7 +30,8 @@ var EventVendorAutocomplete = React.createClass({
                     retrieveData={this.retrieveVendors}
                     data={this.state.vendors}
                     itemSelected={this.addVendorToEvent}
-                    placeholder="Add vendor to event..." />
+                    placeholder="Add vendor to event..."
+                    renderItem={this.renderItem} />
     );
   }
 });
