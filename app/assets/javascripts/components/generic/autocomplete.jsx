2d034367ee06ddaa60fbd872aea8c7c67206696d
JustinK322 <justin@yourplannr.com>
2015-05-11 17:17:27 -0600

    added propTypes

--- app/assets/javascripts/components/generic/autocomplete.jsx
+++ app/assets/javascripts/components/generic/autocomplete.jsx
@@ -1,4 +1,11 @@
 var Autocomplete = React.createClass({
+  propTypes: {
+    itemSelected: React.PropTypes.func,
+    focus: React.PropTypes.bool,
+    retrieveData: React.PropTypes.func,
+    renderItem: React.PropTypes.func,
+    data: React.PropTypes.array
+  },
   getDefaultProps: function() {
     return {
       placeholder: "Start typing..."
