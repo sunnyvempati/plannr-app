53a155b0740e5aa1d5ea450d87546d3ffd72b68b
JustinK322 <justin@yourplannr.com>
2015-05-11 17:18:42 -0600

    refactor: renamed and reorders, new methods with (hopefully) better names

--- app/assets/javascripts/components/specific/vendor_input.jsx
+++ app/assets/javascripts/components/specific/vendor_input.jsx
@@ -1,56 +1,43 @@
 var VendorInput = React.createClass({
   mixins: [Formsy.Mixin, boldAutocompleteItem],
-  getInitialState: function() {
-    return {
-      vendorSelected: false,
-      vendorName: null,
-      vendors: [],
-      focus: false // this is used when you click editAssignedTo
-    };
-  },
-  componentDidMount: function() {
-    var vendorValue = this.getValue();
-    if (vendorValue) {
-      $.get("/vendors/" + vendorValue + ".json", function(result) {
-        if (this.isMounted()) {
-          this.setState({vendorSelected: true, vendorName: result.vendor.name});
-        }
-      }.bind(this));
-    }
-  },
-  getCreateNewVendorItem: function() {
+  //vendor specific
+  getCreateNewAutocompleteItem: function() {
     return {
       name: "Create New Vendor",
       id: -1
     }
   },
-  retrieveVendors: function(term) {
+  //vendor specific
+  retrieveAutocompleteListAsync: function(term) {
     $.post("/vendors/searchEm", {search: {text: term || ""}}, function(result) {
       var vendors = result.vendors;
       if(vendors.length == 0) {
-        vendors.push(this.getCreateNewVendorItem());
+        vendors.push(this.getCreateNewAutocompleteItem());
       }
-      this.setState({vendors: vendors});
+      this.setState({autocompleteList: vendors});
     }.bind(this));
   },
+
   addToForm: function(vendor, term) {
-    var vendor_id = 0, name = "";
-    if (vendor.id == -1) {
-      var payload = {contact: {name: term}};
+    if (vendor.id == this.getCreateNewAutocompleteItem().id) {
+      var payload = {vendor: {name: term}};
       $.post("/vendors.json", payload, function(result) {
-        this.setValue(result.contact.id);
-        this.setState({vendorSelected: true, vendorNAme: result.vendor.name});
+        this.setAutocompleteValue(result.vendor.id, result.vendor.name);
       }.bind(this))
     }
     else {
-      this.setValue(vendor.id);
-      this.setState({vendorSelected: true, vendorName: vendor.name});
+      this.setAutocompleteValue(vendor.id, vendor.name);
     }
   },
-  editVendor: function() {
-    this.setState({vendorSelected: false, vendorName: null, vendors: [], focus: true});
+  setAutocompleteValue: function(id, name) {
+    this.setValue(id);
+    this.setState({autocompleteValueSelected: true, autocompleteSelectedName: name});
   },
-  vendorItem: function(item, term) {
+  editAutocompleteValue: function() {
+    this.setValue(this.getCreateNewAutocompleteItem().id);
+    this.setState({autocompleteValueSelected: false, autocompleteSelectedName: null, autocompleteList: [], autocompleteFocus: true});
+  },
+  renderAutocompleteListItem: function(item, term) {
     var itemName = this.formatMatchedCharacters(item.name, term);
     var cx = React.addons.classSet;
     var itemClasses = cx({
@@ -63,21 +50,32 @@ var VendorInput = React.createClass({
       </div>
     );
   },
-  renderAutocomplete: function() {
-    return (
-      <Autocomplete name={this.props.name}
-                    retrieveData={this.retrieveVendors}
-                    itemSelected={this.addToForm}
-                    data={this.state.vendors}
-                    focus={this.state.focus}
-                    renderItem={this.vendorItem} />
-    );
+  getInitialState: function() {
+    return {
+      autocompleteValueSelected: false,
+      autocompleteSelectedName: null,
+      autocompleteList: [],
+      autocompleteFocus: false // this is used when you click editAssignedTo
+    };
   },
-  renderSelectedVendor: function() {
+  componentDidMount: function() {
+    setAutocompleteValueAsync();
+  },
+  setAutocompleteValueAsync: function() {
+    var vendorId = this.getValue();
+    if (vendorId) {
+      $.get("/vendors/" + vendorId + ".json", function(result) {
+        if (this.isMounted()) {
+          this.setAutocompleteValue(result.vendor.id, result.vendor.name);
+        }
+      }.bind(this));
+    }
+  },
+  renderSelectedAutocompleteItem: function() {
     return (
-      <div className="Autocomplete-picked" onClick={this.editVendor}>
+      <div className="Autocomplete-picked" onClick={this.editAutocompleteValue}>
         <div className="Autocomplete-pickedName">
-          {this.state.vendorName}
+          {this.state.autocompleteSelectedName}
         </div>
         <div className="Autocomplete-edit">
           <i className="fa fa-pencil"></i>
@@ -85,8 +83,18 @@ var VendorInput = React.createClass({
       </div>
     );
   },
+  renderAutocomplete: function() {
+    return (
+      <Autocomplete name={this.props.name}
+                    retrieveData={this.retrieveAutocompleteListAsync}
+                    itemSelected={this.addToForm}
+                    data={this.state.autocompleteList}
+                    focus={this.state.autocompleteFocus}
+                    renderItem={this.renderAutocompleteListItem} />
+    );
+  },
   render: function() {
-    var inputRender = this.state.vendorSelected ? this.renderSelectedVendor() : this.renderAutocomplete();
+    var inputRender = this.state.autocompleteValueSelected ? this.renderSelectedAutocompleteItem() : this.renderAutocomplete();
     return (
       <div className="FormInput">
         <label for={this.props.id}>{this.props.label}</label>
