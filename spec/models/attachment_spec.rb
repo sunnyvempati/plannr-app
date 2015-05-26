a73ff2b8277770f0d41ef1a2825fd8547e76feae
JustinK322 <justin@yourplannr.com>
2015-05-26 11:44:03 -0600

    removed some crap, refactor

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -7,41 +7,59 @@ RSpec.describe Attachment, type: :model do
     include FakeFS::SpecHelpers
 
     context 'for non-production environment' do
-      it 'uploads a tiny test file to dev-bucket on s3' do
+      it 'uploads a 4b test file to dev-bucket on s3' do
+        test_file_name = 'test_file_4b'
         FakeFS.activate!
-        File.open('test_file', 'w') do |f|
-          f.puts('foo') # this is required or uploader_test.file.url will be nil
+        File.open(test_file_name, 'w') do |f|
+          f.puts('foo')
         end
         uploader_test = Attachment.new
-        uploader_test.file_link = File.open('test_file')
+        uploader_test.file_link = File.open(test_file_name)
         uploader_test.save!
         expect(uploader_test.file_link.url).to match /.*\/plannr-first-test.*/
         FakeFS.deactivate!
       end
 
-      it 'uploads a 270KB test file to dev-bucket on s3' do
+      it 'uploads a 350KB test file to dev-bucket on s3' do
+        test_file_name = 'test_file_350KB'
         FakeFS.activate!
-        File.open('test_file', 'w') do |f|
+        File.open(test_file_name, 'w') do |f|
           90000.times do
-            f.puts('foo') # this is required or uploader_test.file.url will be nil
+            f.puts('foo')
           end
         end
         uploader_test = Attachment.new
-        uploader_test.file_link = File.open('test_file')
+        uploader_test.file_link = File.open(test_file_name)
         uploader_test.save!
         expect(uploader_test.file_link.url).to match /.*\/plannr-first-test.*/
         FakeFS.deactivate!
       end
 
-      it 'uploads a 1MB test file to dev-bucket on s3' do
+      it 'uploads a 1.3MB test file to dev-bucket on s3' do
+        test_file_name = 'test_file_1MB'
         FakeFS.activate!
-        File.open('test_file', 'w') do |f|
+        File.open(test_file_name, 'w') do |f|
           360000.times do
-            f.puts('foo') # this is required or uploader_test.file.url will be nil
+            f.puts('foo')
           end
         end
         uploader_test = Attachment.new
-        uploader_test.file_link = File.open('test_file')
+        uploader_test.file_link = File.open(test_file_name)
+        uploader_test.save!
+        expect(uploader_test.file_link.url).to match /.*\/plannr-first-test.*/
+        FakeFS.deactivate!
+      end
+
+      it 'uploads a 11MB test file to dev-bucket on s3' do
+        test_file_name = 'test_file_11MB'
+        FakeFS.activate!
+        File.open(test_file_name, 'w') do |f|
+          3000000.times do
+            f.puts('foo')
+          end
+        end
+        uploader_test = Attachment.new
+        uploader_test.file_link = File.open(test_file_name)
         uploader_test.save!
         expect(uploader_test.file_link.url).to match /.*\/plannr-first-test.*/
         FakeFS.deactivate!
