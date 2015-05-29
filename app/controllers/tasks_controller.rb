0e03e6dc602c210e56cb06d1762a40837df45994
Sunny Vempati <sunnyvempati@gmail.com>
2015-05-29 05:19:54 -0600

    table headers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,6 @@ class TasksController < ApplicationController
   end
 
   def event_tasks
-    binding.pry
     order = sort_params ? "#{sort_params[:entity]} #{sort_params[:order]}" : 'name asc'
     render json: Task.event_tasks(params[:event_id]).order(order)
   end
