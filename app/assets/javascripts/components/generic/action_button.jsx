e0348d3fdf1a9fba1019dc959217f0645430f4f4
JustinK322 <justin@yourplannr.com>
2015-06-05 14:02:37 -0500

    just formatting/indenting

--- app/assets/javascripts/components/generic/action_button.jsx
+++ app/assets/javascripts/components/generic/action_button.jsx
@@ -4,19 +4,19 @@ var ActionButton = React.createClass({
     handleClick: React.PropTypes.func,
     svgClass: React.PropTypes.string
   },
-  render: function() {
+  render: function () {
     var actionButtonClasses = classNames({
       'ActionButton': true,
       'extraPad': this.props.extraPad
     });
     var svgClass = "ActionButton-" + this.props.svgClass;
     return (
-      <div className={actionButtonClasses} onClick={this.props.handleClick}>
-        <div className="ActionButton-svg">
-          <div className={svgClass}></div>
+        <div className={actionButtonClasses} onClick={this.props.handleClick}>
+          <div className="ActionButton-svg">
+            <div className={svgClass}></div>
+          </div>
+          <div className="ActionButton-label">{this.props.label}</div>
         </div>
-        <div className="ActionButton-label">{this.props.label}</div>
-      </div>
     );
   }
 });
