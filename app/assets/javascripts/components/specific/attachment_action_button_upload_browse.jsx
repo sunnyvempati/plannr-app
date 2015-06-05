575f0f56a044c1a7dd52b4969e19a57a33b3f230
JustinK322 <justin@yourplannr.com>
2015-06-05 13:58:10 -0500

    removed unused files

--- app/assets/javascripts/components/specific/attachment_action_button_upload_browse.jsx
+++
@@ -1,9 +0,0 @@
-var AttachmentActionButtonUploadBrowse = React.createClass({
-  render: function () {
-    return (
-        <AttachmentActionButtonUpload {...this.props}>
-          <button className='Button Button--raised Button--primary'>Browse</button>
-        </AttachmentActionButtonUpload>
-    );
-  }
-});
--- app/assets/javascripts/components/specific/attachment_action_button_upload_icon.jsx
+++
@@ -1,9 +0,0 @@
-var AttachmentActionButtonUploadIcon = React.createClass({
-  render: function () {
-    return (
-        <AttachmentActionButtonUpload {...this.props}>
-          <div className="ActionButton-createAttachment"></div>
-        </AttachmentActionButtonUpload>
-    );
-  }
-});
