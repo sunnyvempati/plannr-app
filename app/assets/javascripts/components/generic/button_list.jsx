0a9794e9179a7172416667d2f6e37ba226a70355
Sunny Vempati <sunnyvempati@gmail.com>
2015-06-09 22:30:55 -0600

    form loading icon and fix

--- app/assets/javascripts/components/generic/button_list.jsx
+++ app/assets/javascripts/components/generic/button_list.jsx
@@ -12,20 +12,25 @@ var ButtonList = React.createClass({
     primaryDisabled: React.PropTypes.bool,
     secondaryButtonVisible: React.PropTypes.bool.isRequired,
     secondaryButtonHref: React.PropTypes.string.isRequired,
-    secondaryButtonText: React.PropTypes.string.isRequired
+    secondaryButtonText: React.PropTypes.string.isRequired,
+    primaryLoading: React.PropTypes.bool
+  },
+  loadingIcon: function() {
+    return <i className="fa fa-circle-o-notch fa-spin"></i>;
   },
   render: function() {
     var containerClasses = classNames({
       'ButtonListContainer': true,
       'u-hidden': !this.props.showButtonList
     });
+    var buttonChild = this.props.primaryLoading ? this.loadingIcon() : this.props.primaryButtonText;
     return (
       <div className={containerClasses}>
         <ButtonSecondary isVisible={this.props.secondaryButtonVisible}
                          href={this.props.secondaryButtonHref}
                          buttonText={this.props.secondaryButtonText} />
-        <Button type="submit" disabled={this.props.primaryDisabled}>
-          {this.props.primaryButtonText}
+        <Button type="submit" disabled={this.props.primaryDisabled || this.props.primaryLoading}>
+          {buttonChild}
         </Button>
       </div>
     );
