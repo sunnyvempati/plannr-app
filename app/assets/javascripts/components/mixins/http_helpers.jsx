9f84bfda6f685ff877a282f5cdb8e9b4e7723fa6
JustinK322 <justin@yourplannr.com>
2015-06-29 15:59:04 -0500

    changed "this" to explicitly name the resource file

--- app/assets/javascripts/components/mixins/http_helpers.jsx
+++ app/assets/javascripts/components/mixins/http_helpers.jsx
@@ -1,19 +1,19 @@
 var HttpHelpers = {
   // TODO: When error, toast.
   postToServer: function(url, params, success) {
-    this.showLoading();
+    LoadingToast.showLoading();
     $.post(url, params, function(result) {
-      this.closeLoading();
+      LoadingToast.closeLoading();
       success(result);
     }.bind(this));
   },
   getFromServer: function(url, params, success) {
-    this.showLoading();
+    LoadingToast.showLoading();
     $.get(url, params, function(result) {
       success(result);
-      this.closeLoading();
+      LoadingToast.closeLoading();
     }.bind(this)).fail(function(error) {
-      this.toast("Aw man, something went wrong. Let Plannr know about this!");
+      ToastMessages.toast("Aw man, something went wrong. Let Plannr know about this!");
     }.bind(this));
   }
 }
