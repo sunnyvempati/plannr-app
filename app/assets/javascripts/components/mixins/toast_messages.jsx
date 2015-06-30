8f6db238fc264264aa5e76295c9f5db445423371
JustinK322 <justin@yourplannr.com>
2015-06-30 01:56:27 -0500

    added toastError method to differentiate messages from errors

--- app/assets/javascripts/components/mixins/toast_messages.jsx
+++ app/assets/javascripts/components/mixins/toast_messages.jsx
@@ -1,5 +1,11 @@
+//TODO: allow multiple toast messages - they should pile up
 var ToastMessages = {
   toast: function(message) {
+    var toast = React.createElement(Toast, {message: message});
+    React.render(toast, document.getElementById('toast'));
+    setTimeout(this.closeToast, 3000);
+  },
+  toastError: function(message) {
     var toast = React.createElement(Toast, {message: message});
     React.render(toast, document.getElementById('toast'));
     setTimeout(this.closeToast, 3000);
