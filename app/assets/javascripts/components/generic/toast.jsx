b794e5369bdade21dcc7e4d4a173793d7ba1142a
JustinK322 <justin@yourplannr.com>
2015-06-30 00:46:36 -0500

    added PropTypes and isError prop for styling error toast differently

--- app/assets/javascripts/components/generic/toast.jsx
+++ app/assets/javascripts/components/generic/toast.jsx
@@ -1,13 +1,19 @@
 var Toast = React.createClass({
+  propTypes: {
+    loading: React.PropTypes.bool,
+    isError: React.PropTypes.bool
+  },
   getDefaultProps: function() {
     return {
-      loading: false
+      loading: false,
+      isError: false
     };
   },
   render: function() {
     var toastClasses = classNames({
       'Toast': true,
-      'loading': this.props.loading
+      'loading': this.props.loading,
+      'error': this.props.isError
     })
     return (
       <div className={toastClasses}>
