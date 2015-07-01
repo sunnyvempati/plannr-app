38cb59c281fd95fa61375a5831faed918e170576
Sunny Vempati <sunnyvempati@gmail.com>
2015-07-01 17:05:30 -0600

    removed files

--- app/models/attachment_status.rb
+++ app/models/attachment_status.rb
@@ -1,24 +1,3 @@
 class AttachmentStatus < ActiveRecord::Base
   belongs_to :company
-
-  def self.update_for_uploaded_file(company, file_size)
-    # increment put_count and add to space_count
-    attachment_status = AttachmentStatus.find_or_create_by(company: company)
-    attachment_status.space_count += self.convert_bytes_to_megabytes(file_size) #convert size from bytes to MB
-    attachment_status.put_count += 1
-    attachment_status.save
-  end
-
-  def self.update_for_destroyed_file(company, file_size)
-    # subtract from space_count
-    attachment_status = AttachmentStatus.find_or_create_by(company: company)
-    attachment_status.space_count -= self.convert_bytes_to_megabytes(file_size) #convert size from bytes to MB
-    attachment_status.save
-  end
-
-  private
-
-  def convert_bytes_to_megabytes(bytes)
-    (bytes / MEGABYTE)
-  end
 end
