6f264a13e5fcd10fdae98097e8e337ea2c5ddb17
Sunny Vempati <sunnyvempati@gmail.com>
2015-07-22 00:06:25 -0600

    added description field and fixed comment box

--- app/assets/javascripts/components/event.jsx
+++ app/assets/javascripts/components/event.jsx
@@ -40,54 +40,64 @@ var Event = React.createClass({
     }
     return (
       <div className="EventContainer">
-        <div className="Event-info u-flexGrow-2 Event-client">
-          <div className="Info-header">
-            Client
+        <div className="Event-infoContainer">
+          <div className="Event-info u-flexGrow-2 Event-client">
+            <div className="Info-header">
+              Client
+            </div>
+            <div className="Info-content">
+              <div className="u-bold">{clientName}</div>
+              {clientOrg}<br />
+              {clientEmail}<br />
+              {clientPhone}
+            </div>
           </div>
-          <div className="Info-content">
-            <div className="u-bold">{clientName}</div>
-            {clientOrg}<br />
-            {clientEmail}<br />
-            {clientPhone}
+          <div className="Event-info u-flexGrow-2">
+            <div className="Info-header">
+              Location
+            </div>
+            <div className="Info-content u-bold">
+              {event.location || "Not specified"}
+            </div>
+          </div>
+          <div className="Event-info u-flexGrow-1">
+            <div className="Info-header">
+              Start Date
+            </div>
+            <div className="Info-content u-bold">
+              {this.renderDate(event.start_date)}
+            </div>
+          </div>
+          <div className="Event-info u-flexGrow-1">
+            <div className="Info-header">
+              End Date
+            </div>
+            <div className="Info-content u-bold">
+              {this.renderDate(event.end_date)}
+            </div>
+          </div>
+          <div className="Event-info u-flexGrow-1">
+            <div className="Info-header">
+              Budget
+            </div>
+            <div className="Info-content u-bold">
+              <ReactIntl.FormattedNumber value={event.budget} style="currency" currency="USD" />
+            </div>
+          </div>
+          <div className={editClasses}>
+            <div className="Edit-btn">
+              <button className="Button Button--simple" onClick={this.editEvent.bind(this, event.id)}>
+                Edit
+              </button>
+            </div>
           </div>
         </div>
-        <div className="Event-info u-flexGrow-2">
-          <div className="Info-header">
-            Location
+        <div className="Event-descriptionContainer">
+          <div className="Event-descTitle">
+            Description
           </div>
-          <div className="Info-content u-bold">
-            {event.location || "Not specified"}
-          </div>
-        </div>
-        <div className="Event-info u-flexGrow-1">
-          <div className="Info-header">
-            Start Date
-          </div>
-          <div className="Info-content u-bold">
-            {this.renderDate(event.start_date)}
-          </div>
-        </div>
-        <div className="Event-info u-flexGrow-1">
-          <div className="Info-header">
-            End Date
-          </div>
-          <div className="Info-content u-bold">
-            {this.renderDate(event.end_date)}
-          </div>
-        </div>
-        <div className="Event-info u-flexGrow-1">
-          <div className="Info-header">
-            Budget
-          </div>
-          <div className="Info-content u-bold">
-            <ReactIntl.FormattedNumber value={event.budget} style="currency" currency="USD" />
-          </div>
-        </div>
-        <div className={editClasses}>
-          <div className="Edit-btn">
-            <button className="Button Button--simple" onClick={this.editEvent.bind(this, event.id)}>
-              Edit
-            </button>
+          <div className="Event-desc">
+            {event.description}
           </div>
         </div>
       </div>
