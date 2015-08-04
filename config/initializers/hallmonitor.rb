b332a4a3aa383e8e7345935033e019446c482e08
Chris TenHarmsel <chris@tenharmsel.com>
2015-08-03 21:41:22 -0500

    Slightly modified the metric emitted via hallmonitor so they're a little more clear

--- config/initializers/hallmonitor.rb
+++ config/initializers/hallmonitor.rb
@@ -10,14 +10,18 @@ Hallmonitor.add_outputter Hallmonitor::Outputters::StatsdOutputter.new(prefix, "
 # Set up listeners to auto-monitor action controllers
 ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |name, start, finish, id, data|
   duration = finish - start
-  controller = data[:controller].parameterize
+  controller = data[:controller].gsub('Controller', '').underscore.parameterize
   action = data[:action].parameterize
 
   event_prefix = "controllers.#{controller}.#{action}"
-  status_name = "#{event_prefix}.#{data[:status]}"
+  status_part = data[:status] < 400 ? "success" : "error"
 
+  # Two types of responses, success and error
+  status_name = "#{event_prefix}.#{status_part}"
   Hallmonitor::Event.new(status_name).emit
-  Hallmonitor::TimedEvent.new("#{status_name}.total", duration).emit
-  Hallmonitor::TimedEvent.new("#{status_name}.view", data[:view_runtime]).emit
-  Hallmonitor::TimedEvent.new("#{status_name}.db", data[:db_runtime]).emit
+
+  # Only have general timings, not per response type
+  Hallmonitor::TimedEvent.new("#{event_prefix}.total", duration).emit
+  Hallmonitor::TimedEvent.new("#{event_prefix}.view", data[:view_runtime]).emit
+  Hallmonitor::TimedEvent.new("#{event_prefix}.db", data[:db_runtime]).emit
 end
