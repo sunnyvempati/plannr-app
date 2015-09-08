8a9a885f83297e24c2afe78697d17c45dea8dc69
Sunny Vempati <sunnyvempati@gmail.com>
2015-09-08 10:56:28 -0600

    fixed date picker

--- app/assets/javascripts/components/generic/DatePickerInput.jsx
+++ app/assets/javascripts/components/generic/DatePickerInput.jsx
@@ -26,7 +26,7 @@ var DatePickerInput = React.createClass({
                       dateFormat="MM/DD/YYYY"
                       onChange={this.changeValue}
                       minDate={this.props.minDate}
-                      weekStart="Su"
+                      weekStart="1"
           />
         </div>
         <span className="FormInput-fieldErrorMessage">{this.getErrorMessage()}</span>
